0d9a2bc90da85c3c9af06e20a3abb072f9c521ef
Benjamin DUPUIS <benjamin.dupuis@fr.clara.net>
2023-11-10 09:32:21 +0100

    Add labels

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# opcache_prom_stats
+# Opcache Prometheus Stats
+The goal of this project is to have a native PHP page to display Opcache stats in Prometheus Format and avoid any "Go, Python, ...) exporter.
+
+## Installation
+
+```
+git clone (...)
+cd opcache_stats
+composer install
+```
+
+If you want an artifact
+```
+tar --exclude-vcs -zcvf opcache_stats.tar.gz opcache_stats
+```
+
+## Configuration
+
+### Apache HTTPd Server
+* Example of configuration on localhost with multiple FPM Pool
+
+```
+<VirtualHost *:80>
+    ServerName default
+
+    ServerAdmin root@poil.fr
+
+    DocumentRoot /srv/www/monitoring
+    <Directory /srv/www/monitoring>
+       AllowOverride None
+       Require all granted
+    </Directory>
+
+    <Location "/pl-8.1-test1-opcache-status">
+      Require all denied
+      Require ip 127.0.0.1
+      Alias "/srv/www/monitoring/opcache_stats/index.php"
+      SetHandler "proxy:unix:/run/php/php8.1-fpm.test1.sock|fcgi://test1"
+    </Location>
+
+    <Location "/pl-8.1-test2-opcache-status">
+      Require all denied
+      Require ip 127.0.0.1
+      Alias "/srv/www/monitoring/opcache_stats/index.php"
+      SetHandler "proxy:unix:/run/php/php8.1-fpm.test2.sock|fcgi://test2"
+    </Location>
+</VirtualHost>
+```
+
+### Prometheus / Opentelemetry Agent
+* It looks like mandatory to have a Job per pool because `metrics_path` is per job, if anyone have better idea he can do a PR :)
+```
+- job_name: opcache-test1
+  metrics_path: /pl-8.1-test1-opcache-status
+  scrape_interval: 1m
+  static_configs:
+    - targets: ['127.0.0.1:80']
+- job_name: opcache-test2
+  metrics_path: /pl-8.1-test2-opcache-status
+  scrape_interval: 1m
+  static_configs:
+    - targets: ['127.0.0.1:80']
+```
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Content-Type: text/plain");
 require __DIR__ . '/vendor/autoload.php';
 
 use Prometheus\CollectorRegistry;
@@ -11,92 +11,104 @@ use Prometheus\Counter;
 $adapter = new InMemory();
 $registry = new CollectorRegistry($adapter);
 
-$opcacheMemoryUsed = $registry->registerGauge('opcache', 'memory_used', 'memory used', []);
-$opcacheMemoryFree = $registry->registerGauge('opcache', 'memory_free', 'memory used', []);
-$opcacheMemoryWasted = $registry->registerGauge('opcache', 'memory_wasted', 'memory wasted', []);
-$opcacheMemoryWastedPercentage = $registry->registerGauge('opcache', 'memory_wasted_percentage', 'memory wasted_percentage', []);
+$php_v=PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;
+# Labels building
+if (substr(PHP_SAPI, 0, 3) == 'cgi') {
+  $pool_info = fpm_get_status();
+  $labels = ['php_version', 'php_sapi', 'fpm_pool'];
+  $labels_values = [$php_v, PHP_SAPI, $pool_info['pool']];
+} else {
+  $labels = ['php_version', 'php_sapi'];
+  $labels_values = [$php_v, PHP_SAPI];
+}
 
-$opcacheStringsMemoryUsed = $registry->registerGauge('opcache', 'interned_string_memory_used', 'interned string memory used', []);
-$opcacheStringsMemoryFree = $registry->registerGauge('opcache', 'interned_string_memory_free', 'interned string memory used', []);
+# Span building
+$opcacheMemoryUsed = $registry->registerGauge('opcache', 'memory_used', 'memory used', $labels);
+$opcacheMemoryFree = $registry->registerGauge('opcache', 'memory_free', 'memory used', $labels);
+$opcacheMemoryWasted = $registry->registerGauge('opcache', 'memory_wasted', 'memory wasted', $labels);
+$opcacheMemoryWastedPercentage = $registry->registerGauge('opcache', 'memory_wasted_percentage', 'memory wasted_percentage', $labels);
 
-$opcacheHits = $registry->registerGauge('opcache', 'hits', 'hits', []);
-$opcacheMisses = $registry->registerGauge('opcache', 'misses', 'misses', []);
+$opcacheStringsMemoryUsed = $registry->registerGauge('opcache', 'interned_string_memory_used', 'interned string memory used', $labels);
+$opcacheStringsMemoryFree = $registry->registerGauge('opcache', 'interned_string_memory_free', 'interned string memory used', $labels);
 
-$opcacheCachedScripts = $registry->registerGauge('opcache', 'cached_scripts', 'cached scripts', []);
-$opcacheCachedKeys = $registry->registerGauge('opcache', 'cached_keys', 'cached keys', []);
-$opcacheMaxCachedKeys = $registry->registerGauge('opcache', 'max_cached_keys', 'max cached keys', []);
-$opcacheHitRate = $registry->registerGauge('opcache', 'hit_rate', 'hit_rate', []);
+$opcacheHits = $registry->registerGauge('opcache', 'hits', 'hits', $labels);
+$opcacheMisses = $registry->registerGauge('opcache', 'misses', 'misses', $labels);
+
+$opcacheCachedScripts = $registry->registerGauge('opcache', 'cached_scripts', 'cached scripts', $labels);
+$opcacheCachedKeys = $registry->registerGauge('opcache', 'cached_keys', 'cached keys', $labels);
+$opcacheMaxCachedKeys = $registry->registerGauge('opcache', 'max_cached_keys', 'max cached keys', $labels);
+$opcacheHitRate = $registry->registerGauge('opcache', 'hit_rate', 'hit_rate', $labels);
 
 $opcacheStatus = opcache_get_status();
 
 $opcacheMemoryUsed->set(
     $opcacheStatus['memory_usage']['used_memory'],
-    []
+    $labels_values
 );
 
 $opcacheMemoryFree->set(
     $opcacheStatus['memory_usage']['free_memory'],
-    []
+    $labels_values
 );
 
 $opcacheMemoryWasted->set(
     $opcacheStatus['memory_usage']['wasted_memory'],
-    []
+    $labels_values
 );
 
 $opcacheMemoryWastedPercentage->set(
     $opcacheStatus['memory_usage']['current_wasted_percentage'],
-    []
+    $labels_values
 );
 
 $opcacheStringsMemoryUsed->set(
     $opcacheStatus['interned_strings_usage']['used_memory'],
-    []
+    $labels_values
 );
 
 $opcacheStringsMemoryFree->set(
     $opcacheStatus['interned_strings_usage']['free_memory'],
-    []
+    $labels_values
 );
 
 $opcacheStringsMemoryUsed->set(
     $opcacheStatus['interned_strings_usage']['used_memory'],
-    []
+    $labels_values
 );
 
 $opcacheStringsMemoryFree->set(
     $opcacheStatus['interned_strings_usage']['free_memory'],
-    []
+    $labels_values
 );
 
 $opcacheHits->set(
     $opcacheStatus['opcache_statistics']['hits'],
-    []
+    $labels_values
 );
 
 $opcacheMisses->set(
     $opcacheStatus['opcache_statistics']['misses'],
-    []
+    $labels_values
 );
 
 $opcacheCachedScripts->set(
     $opcacheStatus['opcache_statistics']['num_cached_scripts'],
-    []
+    $labels_values
 );
 
 $opcacheCachedKeys->set(
     $opcacheStatus['opcache_statistics']['num_cached_keys'],
-    []
+    $labels_values
 );
 
 $opcacheMaxCachedKeys->set(
     $opcacheStatus['opcache_statistics']['max_cached_keys'],
-    []
+    $labels_values
 );
 
 $opcacheHitRate->set(
     $opcacheStatus['opcache_statistics']['opcache_hit_rate'],
-    []
+    $labels_values
 );
 
 $renderer = new RenderTextFormat();
